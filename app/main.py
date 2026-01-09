c9427311df218dfe4ef39d63144819122aca7348
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-01-10 01:53:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
 class Person:
-    people = dict()
+    people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    new_people = []
-    for person in people:
-        Person.people[person["name"]] = Person(person["name"], person["age"])
-    for person in people:
-        inst_person = Person.people[person["name"]]
-        if person.get("wife") is not None:
-            inst_person.wife = Person.people.get(person.get("wife"))
-        if person.get("husband") is not None:
-            inst_person.husband = Person.people.get(person.get("husband"))
-        new_people.append(inst_person)
+def create_person_list(people: list) -> list[Person]:
+    person_list = [Person(p.get("name"), p.get("age")) for p in people]
 
-    return new_people
+    for p_dict in people:
+        current_person = Person.people.get(p_dict.get("name"))
+
+        wife_name = p_dict.get("wife")
+        husband_name = p_dict.get("husband")
+
+        if wife_name and Person.people.get(wife_name):
+            current_person.wife = Person.people.get(wife_name)  # type: ignore
+
+        if husband_name and Person.people.get(husband_name):
+            current_person.husband = Person.people.get(husband_name)
+
+    return person_list
